f4feee8cd56a5ad627d8691842ad5cebf4210224
fabiowendel <lfabiowendel@gmail.com>
2021-04-10 19:16:14 -0400

    finalizado HTML,SCSS e TS

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -4,6 +4,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { Project } from '../shared/models/project.model';
 import { ProjectService } from '../shared/services/project/project.service';
 
 @Component({
@@ -15,27 +17,40 @@ export class ProjectComponent implements OnInit {
 
 
   displayedColumns: string[] = ['name', 'date_init', 'date_finish', 'value_project', 'risk_project', 'opcao'];
-  projects: MatTableDataSource<any>;
+  projects: MatTableDataSource<Project>;
 
   userform: FormGroup;
   isLoading = true;
   controlSubmit: boolean = false;
   submitted: boolean;
-  error:any={isError:false,errorMessage:''};
+  error: any = { isError: false, errorMessage: '' };
 
 
 
   @ViewChild('myTemplate') customTemplate: TemplateRef<any>;
+  @ViewChild('myTemplateDelete') customTemplateDelete: TemplateRef<any>;
+  @ViewChild('myTemplateInvestment') customTemplateInvestment: TemplateRef<any>;
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  level_risk: {name:string, value: string; }[];
+  level_risk: { name: string, value: number; }[];
   isValidDate: boolean;
-  totalPages: any;
+  controlDelete: boolean;
+  dataFormItem: any;
+  dataCurrent: any;
+  valueInvestment: number;
+  valueRetorneInvestment: number;
+  riskProject: number;
+  valueProject: number;
+  controlResult: boolean;
+  valueInvestmentFix: number;
+  controlMessageError: boolean;
 
 
   constructor(
     public dialog: MatDialog,
     private fb: FormBuilder,
+    private toastr: ToastrService,
     private projectService: ProjectService,
   ) { }
 
@@ -52,21 +67,21 @@ export class ProjectComponent implements OnInit {
     });
   }
 
-  validateDates(sDate: string, eDate: string){
+  validateDates(sDate: string, eDate: string) {
     this.isValidDate = true;
-    if((sDate == null || eDate ==null)){
-      this.error={isError:true,errorMessage:'A data de início e a data de término são obrigatórias.'};
+    if ((sDate == null || eDate == null)) {
+      this.error = { isError: true, errorMessage: 'A data de início e a data de término são obrigatórias.' };
       this.isValidDate = false;
     }
 
-    if((sDate != null && eDate !=null) && (eDate) < (sDate)){
-      this.error={isError:true,errorMessage:'A data de término deve ser maior que a data de início.'};
+    if ((sDate != null && eDate != null) && (eDate) < (sDate)) {
+      this.error = { isError: true, errorMessage: 'A data de término deve ser maior que a data de início.' };
       this.isValidDate = false;
     }
     return this.isValidDate;
   }
 
-  participants() : FormArray {
+  participants(): FormArray {
     return this.userform.get("participants") as FormArray
   }
 
@@ -79,8 +94,8 @@ export class ProjectComponent implements OnInit {
 
   getItem(): FormGroup {
     return this.fb.group({
-      'name': new FormControl('', Validators.required),
-      'cpf': new FormControl('', Validators.required),
+      'name': new FormControl(this.dataFormItem.name, Validators.required),
+      'cpf': new FormControl(this.dataFormItem.cpf, Validators.required),
     })
   }
 
@@ -88,11 +103,11 @@ export class ProjectComponent implements OnInit {
     this.participants().push(this.newItem());
   }
 
-  editItem(){
+  editItem() {
     this.participants().push(this.getItem());
   }
 
-  removeItem(i:number) {
+  removeItem(i: number) {
     this.participants().removeAt(i);
   }
 
@@ -105,51 +120,164 @@ export class ProjectComponent implements OnInit {
       return;
     }
     console.log(this.userform.value)
-
     if (this.controlSubmit == true) {
+      this.postProject(this.userform.value);
+      this.getProjects();
       this.dialog.closeAll();
     } else if (this.controlSubmit == false) {
+      this.updateProject(this.userform.value);
+      this.getProjects();
       this.dialog.closeAll();
     }
 
   }
 
-  getLevelRisk(){
+  getLevelRisk() {
     this.level_risk = [
-      {name:"Baixo", value: "0"},
-      {name:"Médio",value: "1"},
-      {name:"Alto",value: "2"},
+      { name: "Baixo", value: 0 },
+      { name: "Médio", value: 1 },
+      { name: "Alto", value: 2 },
     ];
   }
 
   getProjects(): void {
     this.projectService.getProjects().subscribe(data => {
-      console.log(data);
       this.projects = new MatTableDataSource(data);
+      this.dataCurrent = data;
       this.projects.sort = this.sort;
+      this.projects.paginator = this.paginator;
       this.isLoading = false;
     }, error => {
-      // this.toastr.error('Falha na comunicação com servidor!', 'Erro!');
+      this.toastr.error('Falha na comunicação com servidor!', 'Erro!');
     });
   }
 
+  postProject(project: Project): void {
+    this.projectService.saveProject(project).subscribe(() => {
+      this.toastr.success('Cadastrado!', 'Sucesso!');
+      this.isLoading = true;
+      this.getProjects();
+    }, err => {
+      this.toastr.error('Falha na comunicação com servidor!', 'Erro!');
+    });
+  }
 
+  deleteProject(): void {
+    let a;
+    this.projectService.deleteProject(this.dataFormItem)
+      .subscribe(() => {
+        this.isLoading = true;
+        this.getProjects();
+        this.dialog.closeAll();
+        this.toastr.success('Deletado!', 'Sucesso!');
+      }, err => {
+        this.toastr.error('Falha na comunicação com servidor!', 'Erro!');
+      });
+  }
+
+  updateProject(project: Project): void {
+    this.projectService.updateProject(project, project)
+      .subscribe((ans) => {
+        this.toastr.success('Atualizado!', 'Sucesso!');
+        this.isLoading = true;
+        this.getProjects();
+      }, err => {
+        this.toastr.error('Falha na comunicação com servidor!', 'Erro!');
+      });
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+    this.projects.filter = filterValue.trim().toLowerCase();
 
+    if (this.projects.paginator) {
+      this.projects.paginator.firstPage();
+    }
   }
 
   openDialog() {
-    this.error={isError:false,errorMessage:''};
+    this.controlSubmit = true;
+    this.controlDelete = false;
+    this.error = { isError: false, errorMessage: '' };
     this.userform.reset();
     this.participants().clear();
     this.addItem();
-    const dialogRef = this.dialog.open(this.customTemplate, {width:'850px'});
+    this.userform.enable()
+    const dialogRef = this.dialog.open(this.customTemplate, { width: '850px' });
   }
 
-  getCurrent(event){
+  openDialogEdit(project) {
+    project.date_init = project.date_init.substring(0, 10);
+    project.date_finish = project.date_finish.substring(0, 10);
+    this.error = { isError: false, errorMessage: '' };
+    this.dataFormItem = project;
+    this.participants().clear();
+    this.dataFormItem.participants.forEach(() => {
+      this.editItem();
+    });
+    this.controlSubmit = false;
+    this.controlDelete = true;
+    this.userform.enable()
+    this.userform.patchValue(project);
+    const dialogRef = this.dialog.open(this.customTemplate, { width: '850px' });
+  }
 
+  openDialogIvenstment(project) {
+    this.controlMessageError = false;
+    this.valueInvestment = null;
+    this.controlResult = false;
+    project.date_init = project.date_init.substring(0, 10);
+    project.date_finish = project.date_finish.substring(0, 10);
+    this.error = { isError: false, errorMessage: '' };
+    this.dataFormItem = project;
+    this.participants().clear();
+    this.dataFormItem.participants.forEach(() => {
+      this.editItem();
+    });
+    this.userform.patchValue(project);
+    this.userform.disable()
+    const dialogRef = this.dialog.open(this.customTemplateInvestment, { width: '850px' });
+
+
+  }
+
+  openDialogDelete() {
+    const dialogRef = this.dialog.open(this.customTemplateDelete);
+  }
+
+  getCurrent(event) {
+    console.log(event.pageIndex)
+    this.isLoading = true;
+    const skip = this.paginator.pageSize * this.paginator.pageIndex;
+    const paged = this.dataCurrent.filter((u, i) => i >= skip)
+      .filter((u, i) => i < this.paginator.pageSize);
+    console.log(paged);
+    this.projects = new MatTableDataSource(paged);
+    this.isLoading = false;
+  }
+
+  validValueInvestment() {
+
+  }
+
+  simulateInvestment() {
+    this.riskProject = +this.dataFormItem.risk_project;
+    this.valueProject = +this.dataFormItem.value_project;
+
+    if (this.valueInvestment > this.valueProject) {
+      this.valueInvestmentFix = +this.valueInvestment;
+      this.controlResult = true;
+      this.controlMessageError = false;
+      if (this.riskProject == 0) {
+        this.valueRetorneInvestment = (5 / 100) * this.valueInvestment;
+      } else if (this.riskProject == 1) {
+        this.valueRetorneInvestment = (10 / 100) * this.valueInvestment;
+      } else {
+        this.valueRetorneInvestment = (20 / 100) * this.valueInvestment;
+      }
+    } else {
+      this.controlMessageError = true;
+    }
   }
 
 
